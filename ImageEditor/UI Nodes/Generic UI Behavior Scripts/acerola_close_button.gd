ec54bfc61acb5c7e759ff4e209e258ded512ac0d
Garrett Gunnell <gunnellg@oregonstate.edu>
2024-12-11 05:14:23 -0800

    Untrack image editor code

--- ImageEditor/UI Nodes/Generic UI Behavior Scripts/acerola_close_button.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-@export var root : Node
-
-func _ready():
-	pressed.connect(self._button_pressed)
-
-func _button_pressed():
-	root.queue_free()
--- ImageEditor/UI Nodes/Generic UI Behavior Scripts/acerola_h_slider.gd
+++
@@ -1,6 +0,0 @@
-@tool
-extends HSlider
-
-
-func _on_line_edit_text_submitted(new_text: String) -> void:
-	value = float(new_text)
